4039a20604fadad7b66630e17b5267ebe60093c4
Haastrup ELijah <haastrupea@gmail.com>
2020-05-12 01:41:53 +0100

    Add sentry to the project: for error management
    Add sentry package to the project as the error tracker via npm

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// import * as Sentry from '@sentry/node';
+// or using CommonJS
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://d4bb1794f5414a76866905f2a7b6f265@o390745.ingest.sentry.io/5235703' });
+
 const app = async () => '#BuildforSDG';
 
 export default app;
